refactor: fix spatial rotation

data/mri_dataset.py
@@ -11,25 +11,12 @@ import torch.nn.functional as F
 from collections.abc import Sequence
 
 class SpatialRotation():
-    def __init__(self, dimensions: Sequence, degree: Sequence = [90.]):
-        
+    def __init__(self, dimensions: Sequence, k: Sequence = [1]):
         self.dimensions = dimensions
-        self.degree = degree
-
-    def invert_permutation_numpy2(self, permutation):
-        # https://stackoverflow.com/a/55737198
-        inv = np.empty_like(permutation)
-        inv[permutation] = np.arange(len(inv), dtype=inv.dtype)
-        return inv
+        self.k = k
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
-        if isinstance(self.dimensions[0], Sequence):
-            dim1, dim2 = random.choice(self.dimensions)
-        else:
-            dim1, dim2 = self.dimensions
-        permutation = [i for i in range(x.dim()) if (i != dim1 and i != dim2)] + [dim1, dim2]
-        reverse_permuation = list(self.invert_permutation_numpy2(permutation))
-        x = transforms.functional.rotate(x.permute(permutation), random.choice(self.degree)).permute(reverse_permuation)
+        x = torch.rot90(x, random.choice(self.k), random.choice(self.dimensions))
         return x
 
 class MRIDataset(BaseDataset):
@@ -42,8 +29,7 @@ class MRIDataset(BaseDataset):
 
         transformations = [
             transforms.Lambda(lambda x: x[48:240,80:240,36:260]), # 192x160x224
-            # transforms.Lambda(lambda x: resize(x, (64,64,75), order=1, anti_aliasing=True)),
-            transforms.Lambda(lambda x: resize(x, (96,80,112), order=1, anti_aliasing=True)),
+            # transforms.Lambda(lambda x: resize(x, (96,80,112), order=1, anti_aliasing=True)),
             transforms.Lambda(lambda x: self.toGrayScale(x)),
             transforms.Lambda(lambda x: torch.tensor(x, dtype=torch.float32)),
             transforms.Lambda(lambda x: x.unsqueeze(0)),
@@ -53,12 +39,12 @@ class MRIDataset(BaseDataset):
 
         if(opt.phase == 'train'):
             transformations += [
-                SpatialRotation([(1,2), (1,3), (2,3)], [0,90,180,270]),
+                SpatialRotation([(1,2), (1,3), (2,3)], [0,1,2,3]),
                 transforms.RandomHorizontalFlip(),
                 transforms.RandomVerticalFlip()
             ]
         else:
-            transformations += [SpatialRotation((1,2))]
+            transformations += [SpatialRotation([(1,2)])]
         self.transform = transforms.Compose(transformations)
 
     def toGrayScale(self, x):

models/networks.py
@@ -1529,24 +1529,25 @@ class ObeliskLayer(nn.Module):
         self.conv2d = nn.Sequential(batchNorm(C_mid+96), conv(C_mid+96,32,1,bias=False), nn.ReLU(True))
         self.conv3 = nn.Sequential(batchNorm(C_mid+128), conv(C_mid+128,C_out,1,bias=False), nn.ReLU(True))
 
-    def create_grid(self, full_res):
-        self.full_res = list(map(lambda x: int(x/self.down_scale_factor), full_res))
-        self.half_res = list(map(lambda x: int(x/(self.down_scale_factor*2)), full_res))
-        self.quarter_res = list(map(lambda x: int(x/(self.down_scale_factor*4)), full_res))
+    def create_grid(self, quarter_res):
+        
         # Obelisk sample_grid: 1 x 1 x #samples x 1 x 3
-        sample_grid = F.affine_grid(torch.eye(3,4).unsqueeze(0), torch.Size((1,1,*self.quarter_res))).view(1,1,-1,*[1]*(dimensions-2),3).detach()
+        sample_grid = F.affine_grid(torch.eye(3,4).unsqueeze(0), torch.Size((1,1,*quarter_res))).view(1,1,-1,*[1]*(dimensions-2),3).detach()
         sample_grid.requires_grad = False
         setattr(self, 'sample_grid', sample_grid)
         self.grid_initialized = True
 
     def forward(self, x: torch.Tensor):
+        half_res = list(map(lambda x: int(x/(self.down_scale_factor*2)), x.shape[2:]))
+        quarter_res = list(map(lambda x: int(x/(self.down_scale_factor*4)), x.shape[2:]))
+
         if not self.grid_initialized:
-            self.create_grid(x.shape[2:])
+            self.create_grid(quarter_res)
         B = x.size()[0]
         device = x.device
 
         # Obelisk Layer
-        x = F.grid_sample(x, (self.sample_grid.to(device).repeat(B,1,*[1]*dimensions) + self.offset), align_corners=True).view(B,-1,*self.quarter_res)
+        x = F.grid_sample(x, (self.sample_grid.to(device).repeat(B,1,*[1]*dimensions) + self.offset), align_corners=True).view(B,-1,*quarter_res)
         x = self.conv1(x)
         # x = self.conv2(x)
 
@@ -1557,7 +1558,7 @@ class ObeliskLayer(nn.Module):
         x = torch.cat([x, self.conv2d(x)], dim=1)
         x = self.conv3(x)
         if self.upscale:
-            x = F.interpolate(x, size=self.half_res, mode='trilinear', align_corners=False)
+            x = F.interpolate(x, size=half_res, mode='trilinear', align_corners=False)
         return x
 
 class ObeliskDiscriminator(nn.Module):
@@ -1620,8 +1621,7 @@ class ObeliskHybridGenerator(nn.Module):
 
     def forward(self, x: torch.Tensor, layers=[], encode_only=False):
         size = x.size()
-        if not hasattr(self, 'half_res'):
-            self.half_res = list(map(lambda x: int(x/2), size[2:]))
+        half_res = list(map(lambda x: int(x/2), size[2:]))
         leakage = 0.05 #leaky ReLU used for conventional CNNs
 
         x0 = self.model0(x)
@@ -1631,21 +1631,22 @@ class ObeliskHybridGenerator(nn.Module):
         x110 = self.model110(x11)
         torch.cuda.empty_cache()
 
-        #unet-decoder
-        x = F.leaky_relu(self.batch6bU(self.conv6bU(torch.cat((x0,x110),1))),leakage)
-        x = F.interpolate(x, size=self.half_res, mode='trilinear', align_corners=False)
-        x = F.leaky_relu(self.batch6U(self.conv6U(torch.cat((x,x10,x11),1))),leakage)
-        x = self.conv8(x)
-        x = F.interpolate(x, size=size[2:], mode='trilinear', align_corners=False)
-
-        all_feats = [x0, x1, x10, x11, x110]
         if encode_only:
+            all_feats = [x0, x1, x10, x11, x110]
             feats = []
             for i,feat in enumerate(all_feats):
                 if i in layers:
                     feats.append(feat)
             return feats
-        
+
+        #unet-decoder
+        x = F.leaky_relu(self.batch6bU(self.conv6bU(torch.cat((x0,x110),1))),leakage)
+        x = F.interpolate(x, size=half_res, mode='trilinear', align_corners=False)
+        x = F.leaky_relu(self.batch6U(self.conv6U(torch.cat((x,x10,x11),1))),leakage)
+        x = self.conv8(x)
+        x = F.interpolate(x, size=size[2:], mode='trilinear', align_corners=False)
+        x = torch.tanh(x)
+
         x = ((x+1)/2)*255.
         x = (x-self.mean) / self.std
         return x