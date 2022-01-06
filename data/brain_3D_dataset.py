fix: rotation invariance + val loss loading

data/brain_3D_dataset.py
@@ -33,14 +33,14 @@ class brain3DDataset(MRIDataset):
         if(opt.phase == 'train'):
             self.updateTransformations += [
                 SpatialRotation([(1,2), (1,3), (2,3)], [0,1,2,3], auto_update=False),
-                SpatialFlip(dims=(1,2,3), auto_update=False),
-                ColorJitter3D((0.3,1.5), (0.3,1,5))
+                SpatialFlip(dims=(1,2,3), auto_update=False)
             ]
         else:
             self.updateTransformations += [SpatialRotation([(1,2)], [2])]
             self.updateTransformations += [SpatialRotation([(1,3)], [1])]
         transformations += self.updateTransformations
         self.transform = transforms.Compose(transformations)
+        self.colorJitter = ColorJitter3D((0.3,1.5), (0.3,1,5))
 
     def __getitem__(self, index):
         A1_path = self.A1_paths[index % self.A_size]  # make sure index is within then range
@@ -56,7 +56,9 @@ class brain3DDataset(MRIDataset):
             B_path = self.B_paths[index_B]
             B_img = np.array(nib.load(B_path).get_fdata())
         A1 = self.transform(A1_img[np.newaxis, ...])
+        A1 = self.colorJitter(A1)
         A2 = self.transform(A2_img[np.newaxis, ...])
+        A2 = self.colorJitter(A2)
         A = torch.concat((A1, A2), dim=0)
         B = self.transform(B_img[np.newaxis, ...])
         return {'A': A, 'B': B, 'A_paths': A1_path, 'B_paths': B_path}

data/data_augmentation_3D.py
@@ -13,12 +13,13 @@ class SpatialRotation():
         self.update()
 
     def update(self):
-        self.args = (random.choice(self.k), random.choice(self.dimensions))
+        self.args = [random.choice(self.k) for dim in self.dimensions]
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         if self.auto_update:
             self.update()
-        x = torch.rot90(x, *self.args)
+        for k, dim in zip(self.args, self.dimensions):
+            x = torch.rot90(x, k, dim)
         return x
 
 class SpatialFlip():

models/pix2pix_model.py
@@ -107,7 +107,7 @@ class Pix2PixModel(BaseModel):
             self.loss_G_perceptual = torch.tensor(0, device=self.opt.gpu_ids[0], dtype=torch.float16)
             if self.opt.perceptual:
                 feats_real = self.netD(torch.cat((self.real_A, self.real_B), 1).detach(), layers=[0, 3, 6, 9], encode_only=True)
-                for i, λ_i in enumerate([5, 1.5, 1.5, 1]):
+                for i, λ_i in enumerate(torch.tensor([5, 1.5, 1.5, 1]).numpy()*1.5):
                     self.loss_G_perceptual += λ_i * self.perceptual_loss(feats_fake[i], feats_real[i])
             # combine loss and calculate gradients
             self.loss_G = self.loss_G_GAN + self.loss_G_L1 + self.loss_G_perceptual

train.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
             validation_loss_array.append(validation_loss_fun(model.fake_B, model.real_B).item())
             validation_loss_array2.append(1 - validation_loss_fun2(model.fake_B, model.real_B).item())
         
-        val_loss = [np.mean(validation_loss_array), np.mean(validation_loss_array2)]
+        val_loss = {'L1': np.mean(validation_loss_array), '1-SSIM': np.mean(validation_loss_array2)}
         visualizer.print_validation_loss(epoch, val_loss)
         visualizer.plot_current_validation_losses(epoch, val_loss)
         opt.phase='train'